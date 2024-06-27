cea0fc14adb81387945f5fd972f3c2a38024e207
gustibilli <bagus.billi@gmail.com>
2024-06-27 17:08:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loadinganimation/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.loadinganimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ProgressBar loadingBar = new ProgressBar(MainActivity.this);
+
+    }
+}
